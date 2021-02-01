096cde19151034b2763cc6fdcbcd8292ad8a7c26
james clarke <jkc200013@gmail.com>
2021-02-01 13:08:19 +0000

    added kprintf

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 
 SOURCES=./src
 BUILD=./build
+CFLAGS="-m32 -ffreestanding -nostdlib -nostdinc -lgcc"
 
 nasm -f elf32  $SOURCES/kernel.asm -o $BUILD/kasm.o
-gcc -m32 -c $SOURCES/kernel.cpp -o $BUILD/kc.o -ffreestanding -nostdlib -nostdinc -lgcc
+gcc $CFLAGS -c $SOURCES/kernel.cpp -o $BUILD/kernel.o
+gcc $CFLAGS -c $SOURCES/kernel/kprintf.cpp -o $BUILD/kprintf.o
 
-ld -m elf_i386 -T $SOURCES/link.ld -o $BUILD/kernel-0 $BUILD/kasm.o $BUILD/kc.o
+ld -m elf_i386 -T $SOURCES/link.ld -o $BUILD/kernel-0 $BUILD/kasm.o $BUILD/kernel.o $BUILD/kprintf.o
 objcopy -O elf32-i386 $BUILD/kernel-0 $BUILD/kernel-0.elf
 qemu-system-i386 -kernel ./build/kernel-0
